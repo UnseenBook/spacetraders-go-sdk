7bad3c81e40f872f0f9ed74750643de570a0c605
Daan Schoone <d.schoone@presspage.com>
2024-02-08 21:20:05 +0100

    feat: add throttler config to http client
    To enable throttling call AddThrottle on Configuration and all API calls
    will be limited to the configured rate limit.

---
+++ config_throttle.go
@@ -0,0 +1,40 @@
+package stsdk
+
+import (
+	"golang.org/x/time/rate"
+	"net/http"
+	"time"
+)
+
+type ThrottledTransport struct {
+	rt http.RoundTripper
+	rl *rate.Limiter
+}
+
+func (tt *ThrottledTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	err := tt.rl.Wait(r.Context())
+	if err != nil {
+		if r.Body != nil {
+			_ = r.Body.Close()
+		}
+		return nil, err
+	}
+
+	return tt.rt.RoundTrip(r)
+}
+
+func (c *Configuration) AddThrottler(period time.Duration, burst int) error {
+	if c.HTTPClient == nil {
+		return reportError("to configure a throttler first add a HTTPClient to the Configuration")
+	}
+	if c.HTTPClient.Transport == nil {
+		return reportError("to configure a throttler first add a Transport to the HTTPClient")
+	}
+	tt := &ThrottledTransport{
+		rt: c.HTTPClient.Transport,
+		rl: rate.NewLimiter(rate.Every(period), burst),
+	}
+	c.HTTPClient.Transport = tt
+
+	return nil
+}
